cbe5c223f81c44f2c08616fe9f1fcba0e36f35e4
erc <erc@student.42.us.org>
2020-08-25 00:03:17 -0700

    feat: final testing

--- pf_solve_x.c
+++ pf_solve_x.c
@@ -6,7 +6,7 @@
 /*   By: erc <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/09 23:40:03 by erc               #+#    #+#             */
-/*   Updated: 2020/08/21 14:03:48 by erc              ###   ########.fr       */
+/*   Updated: 2020/08/25 00:02:40 by erc              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ static	void	pf_flag_checker_x(int strlen, char *str, t_info *finfo)
 		finfo->precision = strlen;
 	if (finfo->flag_zero == 1 && finfo->flag_minus == 1)
 		finfo->flag_zero = 0;
-	if (finfo->flag_zero == 1 && finfo->precision > 0)
+	if (finfo->flag_zero == 1 && finfo->precision >= 0)
 		finfo->flag_zero = 0;
 }
 
