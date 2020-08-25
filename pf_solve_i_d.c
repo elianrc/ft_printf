09e592ff1812d5ef3da4792dcd49cc1415fa8424
erc <erc@student.42.us.org>
2020-08-24 23:56:40 -0700

    feat: final testing

--- pf_solve_i_d.c
+++ pf_solve_i_d.c
@@ -6,7 +6,7 @@
 /*   By: erc <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/09 23:38:56 by erc               #+#    #+#             */
-/*   Updated: 2020/08/24 17:46:14 by erc              ###   ########.fr       */
+/*   Updated: 2020/08/24 23:55:57 by erc              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,8 @@ static	void	pf_flag_checker_id(int strlen, char *str, t_info *finfo)
 		finfo->precision = strlen;
 	if (finfo->flag_zero == 1 && finfo->flag_minus == 1)
 		finfo->flag_zero = 0;
-	if (!IS_MACOS)
-		if (finfo->flag_zero == 1 && finfo->precision >= 0)
-			finfo->flag_zero = 0;
+	if (finfo->flag_zero == 1 && finfo->precision >= 0)
+		finfo->flag_zero = 0;
 	if (finfo->width == strlen && *str != '0')
 		finfo->width = 0;
 }
