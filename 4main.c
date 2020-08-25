ad5a1bab49cb2e44d6e7a5ec5c356f3dd0bbe7f6
erc <erc@student.42.us.org>
2020-08-25 00:24:55 -0700

    feat: final testing

--- 4main.c
+++ 4main.c
@@ -28,8 +28,8 @@ int main(void)
     // ft_printf("|%-5.d|\n", 0);
     // printf("|%-5.d|\n", 0);
 
-    ft_printf("|%0*.*d|\n", 1, 0, 0);
-    printf("|%0*.*d|\n", 1, 0, 0);
+    ft_printf("|%0*.*i|\n", 3, -2, 8);
+    printf("|%0*.*i|\n", 3, -2, 8);
 
 
     return (0);
