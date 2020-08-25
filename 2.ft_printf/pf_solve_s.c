a60633759ac5d26f72a31edff4a3a3707acd6912
erc <erc@student.42.us.org>
2020-08-25 14:29:23 -0700

    feat: improved code organization

--- 2.ft_printf/pf_solve_s.c
+++ 2.ft_printf/pf_solve_s.c
@@ -6,7 +6,7 @@
 /*   By: erc <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/09 23:36:43 by erc               #+#    #+#             */
-/*   Updated: 2020/08/25 14:18:08 by erc              ###   ########.fr       */
+/*   Updated: 2020/08/25 14:28:24 by erc              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 
 #include "ft_printf.h"
 
-static	void	pf_flag_checker_s(int strlen, t_info *finfo)
+static	void	pf_flag_checker_s(int strlen, char *str, t_info *finfo)
 {
-	if (finfo->width <= (strlen  - finfo->precision))
+	if (finfo->width <= (int)ft_strlen(str  - finfo->precision))
 		finfo->width = 0;
 	if (finfo->precision > strlen)
 		finfo->precision = strlen;
@@ -41,7 +41,7 @@ void			pf_solve_string(t_info *finfo)
 	if (finfo->precision == 0)
 		str = "";
 	strlen = (int)ft_strlen(str);
-	pf_flag_checker_s(strlen, finfo);
+	pf_flag_checker_s(strlen, str, finfo);
 	if (finfo->flag_minus == 1)
 		print_minus_true(str, finfo);
 	else
